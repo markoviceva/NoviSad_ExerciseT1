85e56cf21b5d0985a9754419ebb5002cc6fc974b
somename <someemail>
2017-05-11 20:18:33 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -10,16 +10,19 @@ public class BowlingGame {
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
 	
+	
 	public BowlingGame(){}
 	
 	// adds a frame to the game
 	public void addFrame(Frame frame){
+		
 		//to be implemented
 	}
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
 		//to be implemented
+		
 	}
 	
 	// Returns the game score
