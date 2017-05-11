5aa03d77117f204bfecc45511969bb48bf251538
somename <someemail>
2017-05-11 20:58:08 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -43,7 +43,7 @@ public class TestBowling {
 		f.add(new Frame(firstThrow,secondThrow));
 		f.add(new Frame(firstThrow,secondThrow));
 		f.add(new Frame(firstThrow,secondThrow));
-		
+		f.add(new Frame(firstThrow,secondThrow));
 		
 		
 	}
